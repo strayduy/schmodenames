7121eab3ae57576c6c94af0db4df6a21025ca721
Duy Tran <duy@strayharbor.com>
2016-01-01 20:52:59 -0800

    Binding to 0.0.0.0 with the runserver command, so external clients can access the dev server

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 # Third party libs
 from flask.ext.script import Manager
+from flask.ext.script import Server
 
 # Our libs
 from server.app import create_app
@@ -8,6 +9,7 @@ from server.settings import DevConfig
 def main():
     app = create_app(DevConfig)
     manager = Manager(app)
+    manager.add_command('runserver', Server(host='0.0.0.0'))
     manager.run()
 
 if __name__ == '__main__':
