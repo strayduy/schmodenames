e6c441b5bf9d995f030099a1c9d0de1d0aa3ee64
Duy Tran <duy@strayharbor.com>
2016-01-01 22:31:01 -0800

    Minifying the client app JS file

--- client/gulpfile.babel.js
+++ client/gulpfile.babel.js
@@ -2,9 +2,14 @@ const gulp = require('gulp');
 
 const babelify   = require('babelify');
 const browserify = require('browserify');
+const rename     = require('gulp-rename');
 const source     = require('vinyl-source-stream');
+const streamify  = require('gulp-streamify');
+const uglify     = require('gulp-uglify');
 
 gulp.task('browserify', () => {
+    let dest = 'static';
+
     browserify({
         entries: 'src/app/index.jsx',
         extensions: ['.js', '.jsx'],
@@ -12,7 +17,10 @@ gulp.task('browserify', () => {
     .transform('babelify')
     .bundle()
     .pipe(source('index.js'))
-    .pipe(gulp.dest('static'));
+    .pipe(gulp.dest(dest))
+    .pipe(streamify(uglify()))
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest(dest));
 });
 
 gulp.task('vendor', () => {
